1fe47558f1add488aa39346fe00835f74c11eb33
nikola719 <nkjanko776@gmail.com>
2021-01-29 13:52:57 +0100

    second push

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,30 +5,6 @@ $(document).ready(function () {
   // Wow init
   new WOW().init();
 
-  // Init scroll spy
-  // $(".header-nav__link").click(function () {
-  //   let target = $(this).attr("href");
-  //   if (target == "#start") {
-  //     $("html, body").animate(
-  //       {
-  //         scrollTop: 0,
-  //       },
-  //       800
-  //     );
-  //     $(".header").removeClass("active");
-  //     $(".hamburger").removeClass("active");
-  //   } else {
-  //     $("html, body").animate(
-  //       {
-  //         scrollTop: $(target).offset().top,
-  //       },
-  //       800
-  //     );
-  //     $(".header").removeClass("active");
-  //     $(".hamburger").removeClass("active");
-  //   }
-  // });
-
   // Hamburger
   $(".hamburger").click(function () {
     $(this).toggleClass("active");
@@ -46,4 +22,77 @@ $(document).ready(function () {
     if (scrollTop > 50) $(".header").addClass("is-scrolling");
     else $(".header").removeClass("is-scrolling");
   });
+
+  //filter select
+  $(".article-filter__item").click(function () {
+    $(this).addClass("filtered");
+  });
+  $(".filter-cancel").click(function (e) {
+    e.stopPropagation();
+    $(this).parent().removeClass("filtered");
+  });
+
+  //podcasts play movie
+  $(".podcast-carousel__details .start img").click(function (e) {
+    e.stopPropagation();
+    $(this).closest(".podcast-carousel__single").addClass("active");
+  });
+  $(".podcast-carousel__single .stop").click(function (e) {
+    e.stopPropagation();
+    $(this).closest(".podcast-carousel__single").removeClass("active");
+  });
+
+  if ($(".podcast-carousel").length) {
+    $(".podcast-carousel .podcast-carousel__container").slick({
+      arrows: true,
+      // dots: true,
+      // speed: 1500,
+      cssEase: "ease",
+      easing: "linear",
+      autoplay: false,
+      infinite: true,
+      autoplaySpeed: 3000,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 560,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+    blog_posts_height_adjust();
+    $(window).on("resize", function () {
+      blog_posts_height_adjust();
+    });
+  }
+  function blog_posts_height_adjust() {
+    var max_height = 0;
+    $(".podcast-carousel__container .podcast-carousel__single").each(
+      function () {
+        if (max_height < $(this).height()) {
+          max_height = $(this).height();
+        }
+      }
+    );
+    $(".podcast-carousel__container .podcast-carousel__single").each(
+      function () {
+        $(this).height(max_height);
+      }
+    );
+  }
 });
